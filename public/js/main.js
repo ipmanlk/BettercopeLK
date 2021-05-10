6a2661b248cd5d2d6b761aced25fa3c75bb39d1f
ipmanlk <io@navinda.xyz>
2021-05-10 21:13:25 +0530

    add: new ui

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,16 @@
 let searchStatus = false;
-const imgLoading = document.getElementById("imgLoading");
 
 document.querySelector("form").addEventListener("submit", (e) => {
 	e.preventDefault();
-
 	if (searchStatus) return;
 
-	const keyword = document.getElementById("txtKeyword").value;
+	const keyword = document.querySelector(".search-bar").value;
+	const searchResults = document.querySelector(".s-results");
+	const loadingHTML = `<center><img class="spinner-img" src="./img/loading.gif" width="100px" /></center>`;
+	searchResults.innerHTML = loadingHTML;
+	searchResults.classList.remove("hidden");
 
-	searchResults.innerHTML = "";
 	searchStatus = true;
-	imgLoading.className = "visible";
 
 	fetch(`./search/${keyword}`)
 		.then(async (res) => {
@@ -21,36 +21,22 @@ document.querySelector("form").addEventListener("submit", (e) => {
 				return;
 			}
 
-			const searchResults = document.getElementById("searchResults");
-
+			searchResults.innerHTML = "";
+			let html = "";
 			response.data.forEach((result) => {
-				searchResults.innerHTML += `
-            <div class="panel panel-default">
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <img src="${result.thumbnail}"
-                                    width="100%">
-                                </div>
-                                <div class="col-md-8">
-                                    <h4>${result.title}</h4>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel-footer">
-                            <span onClick="downloadSub('${result.postUrl}')" class="btn btn-primary btn-block">Download</span>
-                        </div>
-                    </div>
-            `;
+				html += `
+				<div class="s-result">
+					<h3 onClick="downloadSub('${result.postUrl}')">${result.title}</h3>
+				</div>`;
 			});
-
+			searchResults.innerHTML = html;
 			searchStatus = false;
-			imgLoading.className = "hidden";
 		})
 		.catch((e) => {
+			searchResults.classList.add("hidden");
+			console.log(e);
 			window.alert("Unable to reach the api.");
 			searchStatus = false;
-			imgLoading.className = "hidden";
 		});
 });
 
