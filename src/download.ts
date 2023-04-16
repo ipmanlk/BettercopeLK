ad407bce72a5d0ebd2e2cbd0cfde9557de60722c
ipmanlk <io@navinda.xyz>
2023-04-16 12:26:44 +0530

    fix: baiscopelk invalid filenames

--- src/download.ts
+++ src/download.ts
@@ -1,50 +1,52 @@
 import { load } from "cheerio";
 import { request } from "undici";
 import { Source } from "./types";
-import { getFilename } from "./util";
+import { getBaiscopelkFilename, getFilenameFromUrl } from "./util";
 
 const downloadBaiscopelk = async (postUrl: string) => {
-	// extract html from the webpage
-	const { body } = await request(postUrl);
-	const html = await body.text();
+  // extract html from the webpage
+  const { body } = await request(postUrl);
+  const html = await body.text();
 
-	// find download link
-	const $ = load(html);
-	const dLink = $("img[src='https://baiscopelk.com/download.png']")
-		.parent()
-		.attr("href") as string;
+  // find download link
+  const $ = load(html);
+  const dLink = $("img[src='https://baiscopelk.com/download.png']")
+    .parent()
+    .attr("href") as string;
 
-	// download sub to memory
-	const { body: downloadBody } = await request(dLink, { method: "POST" });
+  // download sub to memory
+  const { body: downloadBody, headers } = await request(dLink, {
+    method: "POST",
+  });
 
-	// return it as a buffer
-	return {
-		filename: getFilename(dLink),
-		file: Buffer.from(await downloadBody.arrayBuffer()),
-	};
+  // return it as a buffer
+  return {
+    filename: getBaiscopelkFilename(`${headers["content-disposition"]}`),
+    file: Buffer.from(await downloadBody.arrayBuffer()),
+  };
 };
 
 const downloadCineru = async (postUrl: string) => {
-	const { body } = await request(postUrl);
-	const html = await body.text();
+  const { body } = await request(postUrl);
+  const html = await body.text();
 
-	const $ = load(html);
-	const dLink = $("#btn-download").data("link") as string;
+  const $ = load(html);
+  const dLink = $("#btn-download").data("link") as string;
 
-	const { body: downloadBody } = await request(dLink);
+  const { body: downloadBody } = await request(dLink);
 
-	return {
-		filename: getFilename(dLink),
-		file: Buffer.from(await downloadBody.arrayBuffer()),
-	};
+  return {
+    filename: getFilenameFromUrl(dLink),
+    file: Buffer.from(await downloadBody.arrayBuffer()),
+  };
 };
 
 export const downloadSubtitle = async (postUrl: string, source: Source) => {
-	switch (source) {
-		case "baiscopelk":
-			return downloadBaiscopelk(postUrl);
+  switch (source) {
+    case "baiscopelk":
+      return downloadBaiscopelk(postUrl);
 
-		case "cineru":
-			return downloadCineru(postUrl);
-	}
+    case "cineru":
+      return downloadCineru(postUrl);
+  }
 };
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,12 @@
-export const getFilename = (url: string) => {
-	const filename = url.trim().replace(/\/$/, "").split("/").pop();
-	if (!filename) return "subtitle.zip";
-	return !/.+\..{1,4}$/.test(filename) ? `${filename}.zip` : filename;
+const BAISCOPELK_REGEX = /filename\*=UTF-8''([^;]+)/;
+
+export const getBaiscopelkFilename = (contentDisposition: string) => {
+  const result = contentDisposition.match(BAISCOPELK_REGEX);
+  return result ? result[1] : "subtitle.zip";
+};
+
+export const getFilenameFromUrl = (url: string) => {
+  const filename = url.trim().replace(/\/$/, "").split("/").pop();
+  if (!filename) return "subtitle.zip";
+  return !/.+\..{1,4}$/.test(filename) ? `${filename}.zip` : filename;
 };
