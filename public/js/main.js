84bfcff048e965ea973a5e004591018d3684418e
ipmanlk <io@navinda.xyz>
2023-09-17 13:59:57 +0530

    refactor: client side js

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,3 @@
-let searchStatus = false;
-
 const sourceNames = {
   baiscopelk: "Baiscope.lk",
   cineru: "Cineru.lk",
@@ -7,72 +5,78 @@ const sourceNames = {
 };
 
 const loadingHTML = `<center><img class="spinner-img" src="./img/loading.gif" width="100px" /></center>`;
+const searchForm = document.querySelector("form");
+const searchResults = document.querySelector(".s-results");
+const searchBar = document.querySelector(".search-bar");
 
-document.querySelector("form").addEventListener("submit", (e) => {
+let searchStatus = false;
+let eventSource = null;
+
+searchForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   if (searchStatus) return;
-  searchSubtitles();
-});
 
-const searchSubtitles = async () => {
-  const keyword = document.querySelector(".search-bar").value;
-  const searchResults = document.querySelector(".s-results");
-
+  const keyword = searchBar.value;
   searchResults.innerHTML = loadingHTML;
   searchResults.classList.remove("hidden");
-
   searchStatus = true;
 
-  const eventSource = new EventSource(encodeURI(`/search?keyword=${keyword}`));
+  try {
+    eventSource = new EventSource(encodeURI(`/search?keyword=${keyword}`));
+    eventSource.addEventListener("result", handleResult);
+    eventSource.addEventListener("error", handleError);
+    eventSource.addEventListener("end", handleEnd);
+  } catch (error) {
+    console.error("Error:", error);
+    handleError();
+  }
+});
 
-  eventSource.addEventListener("result", (event) => {
-    console.debug("Result: ", event);
+const handleResult = (event) => {
+  console.debug("Result: ", event);
+  const data = JSON.parse(event.data);
 
-    const data = JSON.parse(event.data);
-
-    if (data.error) {
-      swal("Error!", data.error, "error");
-      resetState();
-      return;
-    }
-
-    if (!data?.length) return;
-    if (searchResults.innerHTML.includes("loading.gif")) {
-      searchResults.innerHTML = "";
-    }
-
-    const listItems = [];
-
-    data.forEach((result) => {
-      listItems.push(`
-			<div class="s-result" onClick="downloadSub('${result.postUrl}', '${
-        result.source
-      }')">
-				<h3>${result.title}</h3>
-				<h3 class="download-source">${sourceNames[result.source]}</h3>
-				<img src="./img/down.svg" class="download-icon" />
-			</div>`);
-    });
-
-    searchResults.innerHTML += listItems.join("");
-  });
-
-  eventSource.addEventListener("error", (event) => {
-    console.error("Error: ", event);
-    eventSource.close();
-    searchStatus = false;
+  if (data.error) {
+    swal("Error!", data.error, "error");
     resetState();
+    return;
+  }
+
+  if (!data?.length) return;
+  if (searchResults.innerHTML.includes("loading.gif")) {
+    searchResults.innerHTML = "";
+  }
+
+  const listItems = data.map((result) => {
+    return `
+      <div class="s-result" onClick="downloadSub('${result.postUrl}', '${result.source}')">
+        <h3>${result.title}</h3>
+        <h3 class="download-source">${sourceNames[result.source]}</h3>
+        <img src="./img/down.svg" class="download-icon" />
+      </div>`;
   });
 
-  eventSource.addEventListener("end", (event) => {
-    console.debug("End: ", event);
+  searchResults.innerHTML += listItems.join("");
+};
+
+const handleError = () => {
+  console.error("Error: ");
+  if (eventSource) {
     eventSource.close();
-    searchStatus = false;
-  });
+  }
+  searchStatus = false;
+};
+
+const handleEnd = () => {
+  console.debug("End: ");
+  if (eventSource) {
+    eventSource.close();
+  }
+  searchStatus = false;
 };
 
 const downloadSub = (postUrl, source) => {
-  swal("Success!", "Subtitle will start download shortly.", "success");
+  swal("Success!", "Subtitle will start downloading shortly.", "success");
   document.getElementById("txtPostUrl").value = postUrl;
   document.getElementById("txtSource").value = source;
   setTimeout(() => {
@@ -81,7 +85,7 @@ const downloadSub = (postUrl, source) => {
 };
 
 const resetState = () => {
-  document.querySelector(".s-results").classList.add("hidden");
-  document.querySelector(".s-results").innerHTML = "";
-  document.querySelector(".search-bar").value = "";
+  searchResults.classList.add("hidden");
+  searchResults.innerHTML = "";
+  searchBar.value = "";
 };
