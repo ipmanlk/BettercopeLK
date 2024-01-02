5f42812019fcb5e3416e60eebf4f932ad52816f9
ipmanlk <io@navinda.xyz>
2024-01-02 23:30:12 +0530

    migrated UI to preact

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,3 +1,11 @@
+import { h, render } from "https://esm.sh/preact";
+import { useState } from "https://esm.sh/preact/hooks";
+import htm from "https://esm.sh/htm";
+
+// Initialize htm with Preact
+const html = htm.bind(h);
+
+// Humanized source names
 const sourceNames = {
   baiscopelk: "Baiscope.lk",
   cineru: "Cineru.lk",
@@ -5,92 +13,113 @@ const sourceNames = {
   zoomlk: "Zoom.lk",
 };
 
-const loadingHTML = `<center><img class="spinner-img" src="./img/loading.gif" width="100px" /></center>`;
-const searchForm = document.querySelector("form");
-const searchResults = document.querySelector(".s-results");
-const searchBar = document.querySelector(".search-bar");
+function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-let searchStatus = false;
-let eventSource = null;
+  return html`
+    <div class="logo">
+      <h1>BettercopeLK</h1>
+      <i>Better way to search & download Sinhala subtitles</i>
+    </div>
 
-searchForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  if (searchStatus) return;
+    ${SearchBox({
+      setSearchResults,
+      setIsLoading,
+      isLoading,
+    })}
+    ${ResultsBox({
+      results: searchResults,
+      isLoading,
+    })}
+  `;
+}
 
-  const keyword = searchBar.value;
-  searchResults.innerHTML = loadingHTML;
-  searchResults.classList.remove("hidden");
-  searchStatus = true;
+function SearchBox({ setSearchResults, setIsLoading, isLoading }) {
+  const [query, setQuery] = useState("");
 
-  eventSource = new EventSource(encodeURI(`/api/search?query=${keyword}`));
-  eventSource.addEventListener("results", handleResults);
-  eventSource.addEventListener("error", handleError);
-  eventSource.addEventListener("end", handleEnd);
-});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isLoading) return;
+    setIsLoading(true);
 
-const handleResults = (event) => {
-  console.debug("Results: ", event);
-  const data = JSON.parse(event.data);
+    const eventSource = new EventSource(
+      encodeURI(`/api/search?query=${query}`)
+    );
+    eventSource.addEventListener("results", (event) => {
+      console.debug("Results: ", event);
+      try {
+        const data = JSON.parse(event.data);
+        setSearchResults((prev) => [...prev, ...data]);
+      } catch (e) {
+        console.error(e);
+      }
+    });
+    eventSource.addEventListener("error", (event) => {
+      console.debug("Error: ", event);
+      if (event.data === "MISSING_QUERY") {
+        swal("Error!", "Please enter a keyword to search.", "error");
+        resetState();
+      }
+      if (eventSource) {
+        eventSource.close();
+      }
+      setIsLoading(false);
+    });
+    eventSource.addEventListener("end", (event) => {
+      console.debug("End: ", event);
+      if (eventSource) {
+        eventSource.close();
+      }
+      setIsLoading(false);
+    });
+  };
 
-  if (data.error) {
-    swal("Error!", data.error, "error");
-    resetState();
-    return;
-  }
+  return html`
+    <div class="controls-holder">
+      <form class="controls" onSubmit=${handleSubmit}>
+        <input
+          type="text"
+          placeholder="Type your keyword here..."
+          class="search-bar"
+          value=${query}
+          onInput=${(e) => setQuery(e.target.value)}
+        />
+        <input type="submit" value="Search" class="search-btn" />
+      </form>
+    </div>
+  `;
+}
 
-  if (!data?.length) return;
-  if (searchResults.innerHTML.includes("loading.gif")) {
-    searchResults.innerHTML = "";
-  }
+function ResultsBox({ results, isLoading }) {
+  const handleDownload = (url, source) => {
+    swal("Success!", "Subtitle will start downloading shortly.", "success");
+    window.open(`/api/download?postUrl=${url}&source=${source}`, "_blank");
+  };
 
-  const listItems = data.map((result) => {
-    return `
-      <div class="s-result" onClick="downloadSub('${result.postUrl}', '${
-      result.source
-    }')">
-        <h3>${result.title}</h3>
-        <h3 class="download-source">${sourceNames[result.source]}</h3>
-        <img src="./img/down.svg" class="download-icon" />
-      </div>`;
-  });
+  return html`
+    <div class="s-results-holder">
+      <div class="s-results ${!isLoading && !results.length && "hidden"}">
+        ${isLoading && !results.length
+          ? html`<center>
+              <img class="spinner-img" src="./img/loading.gif" width="100px" />
+            </center>`
+          : ""}
+        ${results.map(
+          (result) => html`
+            <div
+              class="s-result"
+              onClick=${() => handleDownload(result.postUrl, result.source)}
+            >
+              <h3>${result.title}</h3>
+              <h3 class="download-source">${sourceNames[result.source]}</h3>
+              <img src="./img/down.svg" class="download-icon" />
+            </div>
+          `
+        )}
+      </div>
+    </div>
+  `;
+}
 
-  searchResults.innerHTML += listItems.join("");
-};
-
-const handleError = (event) => {
-  console.debug("Error: ", event);
-
-  if (event.data === "MISSING_QUERY") {
-    swal("Error!", "Please enter a keyword to search.", "error");
-    resetState();
-  }
-
-  if (eventSource) {
-    eventSource.close();
-  }
-  searchStatus = false;
-};
-
-const handleEnd = (event) => {
-  console.debug("End: ", event);
-  if (eventSource) {
-    eventSource.close();
-  }
-  searchStatus = false;
-
-  if (searchResults.innerHTML.includes("loading.gif")) {
-    resetState();
-    swal("Sorry!", "No results found.", "error");
-  }
-};
-
-const downloadSub = (postUrl, source) => {
-  swal("Success!", "Subtitle will start downloading shortly.", "success");
-  window.open(`/api/download?postUrl=${postUrl}&source=${source}`, "_blank");
-};
-
-const resetState = () => {
-  searchResults.classList.add("hidden");
-  searchResults.innerHTML = "";
-  searchBar.value = "";
-};
+render(html`<${App} />`, document.getElementById("app"));
